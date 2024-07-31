84e978ee553b5f129466cbe85f58bc017bf05685
Bryan Meurisse <meurisse.bryan@gmail.com>
2024-07-31 11:23:56 -0700

    fix foreign type

--- src/FieldTypes/Foreign.php
+++ src/FieldTypes/Foreign.php
@@ -8,18 +8,19 @@ class Foreign
 {
     public static function prepare($name, $options = [], $value = null, $properties = [])
     {
+        // Define the total of columns
         $colsSize = 12 / $properties['config']['cols'];
 
         $html = '<div class="row i-checks">';
         foreach ($options as $key => $option) {
-            $uniqueId = uniqid('opt', true) . md5(mt_rand(1, 1000));
+            $uniqueId = uniqid('opt', true).md5(mt_rand(1, 1000));
+            $checkbox = Html::checkbox($name.'[]', in_array($key, (array) $value), $key)
+                ->id($uniqueId);
+            $label = Html::label($option)->for($uniqueId);
             $html .=
-                '<div class="col-md-' . $colsSize . ' col-xs-12">' .
-                Html::checkbox($name . '[]', in_array($key, (array) $value))
-                    ->id($uniqueId)
-                    ->value($key)
-                    ->toHtml() .
-                Html::label($uniqueId, $option)->toHtml() .
+                '<div class="col-md-'.$colsSize.' col-xs-12">'.
+                $checkbox.
+                $label.
                 '</div>';
         }
         $html .= '</div>';
